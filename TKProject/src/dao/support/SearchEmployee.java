f814225fa3350571f2bc89d0c25552476085af2b
kimjoin2@gmail.com <kimjoin2@gmail.com>
2017-06-05 23:49:30 +0900

    merge with latest remote version

---
+++ TKProject/src/dao/support/SearchEmployee.java
@@ -0,0 +1,11 @@
+package dao.support;
+
+/**
+ * Created by bumskim on 2017. 6. 5..
+ */
+public class SearchEmployee {
+    static final public int SEARCH_CODE = 1;
+    static final public int SEARCH_KANJI = 2;
+    static final public int SEARCH_NAME = 3;
+    static final public int SEARCH_SECTION = 4;
+}
