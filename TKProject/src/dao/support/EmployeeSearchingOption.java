eba5e83bb2f976a6761f0fcf6e65069218f90fdc
Donghoon Im <dongttang@gmail.com>
2017-06-06 13:45:25 +0900

    rename class

---
+++ TKProject/src/dao/support/EmployeeSearchingOption.java
@@ -0,0 +1,18 @@
+package dao.support;
+
+/**
+ * Created by bumskim on 2017. 6. 5..
+ */
+public class EmployeeSearchingOption {
+
+    static final public int SEARCH_BY_DEFAULT = 0;
+
+    static final public int SEARCH_BY_CODE = 1;
+
+    static final public int SEARCH_BY_NAME = 2;
+
+    static final public int SEARCH_BY_KANA = 3;
+
+    static final public int SEARCH_BY_SECTION = 4;
+
+}
